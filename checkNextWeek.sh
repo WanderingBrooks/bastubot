43696202c65c5eb8a45b3877fca90bc2ce3333dc
Jason Brooks <cs.jasonbrooks@gmail.com>
2026-01-31 19:30:59 +0100

    Move logging responsibility into JS using winston with a rotation

--- checkNextWeek.sh
+++ checkNextWeek.sh
@@ -8,8 +8,4 @@ set -a  # Automatically export variables
 source "$SCRIPT_DIR/.env"
 set +a  # Disable automatic export
 
-# Make the logs directory if it doesn't exist
-mkdir -p "/tmp/bastubot"
-mkdir -p "/tmp/bastubot/next-week"
-
-$(which node) $SCRIPT_DIR/dist/index.js 'nextWeek' >> /tmp/bastubot/next-week/log_$(date +'%Y-%m-%d_%H-%M').log 2>&1;
+$(which node) $SCRIPT_DIR/dist/index.js 'nextWeek'
